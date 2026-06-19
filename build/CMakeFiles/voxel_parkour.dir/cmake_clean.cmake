7bc094a45afba1751106ac88a745552f1b4f2646
RyanXPS <ralport2005@gmail.com>
2026-06-19 15:45:16 -0400

    one shotted little voxel game with composer 2.5

---
+++ build/CMakeFiles/voxel_parkour.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/voxel_parkour.dir/main.c.obj"
+  "CMakeFiles/voxel_parkour.dir/main.c.obj.d"
+  "libvoxel_parkour.dll.a"
+  "voxel_parkour.exe"
+  "voxel_parkour.exe.manifest"
+  "voxel_parkour.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/voxel_parkour.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/voxel_parkour.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for voxel_parkour.
+# This may be replaced when dependencies are built.
